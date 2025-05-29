8a562622c16447db15f51fedce2a4a61a43a872c
Alissa Tsay <tsaya@union.edu>
2025-05-28 20:11:54 -0400

    Replace AuthProvider.jsx

---
+++ kaat/src/components/AuthProvider.jsx
@@ -0,0 +1,22 @@
+// AuthProvider.js
+import { GoogleLogin } from '@react-oauth/google';
+
+export default function AuthProvider({ setIsLoggedIn, setUser }) {
+  return (
+    <div className="login-page">
+      <h1>Please sign in</h1>
+
+      <GoogleLogin
+        onSuccess={({ credential }) => {
+          const payload = JSON.parse(atob(credential.split('.')[1]));
+          setUser(payload);
+          setIsLoggedIn(true);
+          // TODO: later -> POST credential to /api/auth/google
+        }}
+        onError={() => console.log('Login failed')}
+        theme="outline"
+        size="large"
+      />
+    </div>
+  );
+}
